6527f861e5e50ea402710b69301ceaece089a2bf
Aaron Anderson <aaronlynanderson@gmail.com>
2019-05-19 01:18:48 -0400

    conditional render function is ready

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import WyreComponent from './components/WyreComponent'
 
 class ReactEtherButton extends React.Component {
-  // renderWyre = () => {
-  //   return <WyreComponent />
-  // }
+  renderWyre = () => {
+    return <WyreComponent />
+  }
 
   render() {
     return (
       <div>
         <button>The button!</button>
-        <WyreComponent />
+        {this.renderWyre()}
       </div>
     )
   }
