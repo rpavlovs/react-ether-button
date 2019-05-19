5fa68a673ca303631cdbb936d16ecc33ac7baa9c
Roman Pavlovskyi <roman@pumpup.co>
2019-05-19 00:58:30 -0400

    :star: [Emotion] Add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class ReactEtherButton extends React.Component {
   render() {
-    return <button>The button!</button>
+    return <button>The button!!</button>
   }
 }
 
