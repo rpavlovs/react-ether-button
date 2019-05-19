284eeed488df33498ac8988d46087c861eedb0d5
Aaron Anderson <aaronlynanderson@gmail.com>
2019-05-19 01:57:28 -0400

    node: fs set to empty

--- src/components/WyreComponent/WyreComponent.js
+++ src/components/WyreComponent/WyreComponent.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+const WyreClient = require('@wyre/api').WyreClient
+
+let wyre = new WyreClient({
+  format: 'json_numberstring',
+  apiKey: 'AK-YWDRRUMN-X9HJQUNF-ZLPMZBLH-MF9RGU99',
+  secretKey: 'SK-B4UXUTJX-JATVLDFT-TENNTPBV-3WTQ67WV',
+  baseUrl: 'https://api.testwyre.com',
+})
+
+// wyre.get('/v2/account').then(
+//   account => {
+//     console.log('I am Wyre account ', account.id)
+//   },
+//   err => {
+//     console.log("Problems, cap'n: ", err)
+//   }
+// )
+
 class WyreComponent extends React.Component {
   render() {
-    return <button>This is the Wyre Component</button>
+    return <div>this is the wyre component</div>
   }
 }
 
